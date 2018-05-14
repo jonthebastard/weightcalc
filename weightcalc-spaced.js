f44fd59edb653767ba6d3b7d8de0d0f4e8aa3647
jonathan.pirro <jonathan.pirro@wbgames.com>
2018-05-14 08:16:34 -0700

    comment and variable cleanup

--- weightcalc-spaced.js
+++ weightcalc-spaced.js
@@ -11,7 +11,11 @@ var plateInventory = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Inven
 
 function weightCalc(setWeight)
 {
-
+  // calculate weight total per side: (total - bar) / 2
+  var goalSide = parseFloat((setWeight - 45) / 2);
+  var totalSide = parseFloat(0);
+  var sidePlates = [];
+  
   // build remainingPlates as a list from plateInventory: weight x count
   var remainingPlates = [];
   for (var i = 1 ; i < plateInventory.length; i++) {
@@ -19,16 +23,8 @@ function weightCalc(setWeight)
       remainingPlates.push(plateInventory[i][0]);
     }
   }
-  // calculate weight total per side: (total - bar) / 2
-  var goalSide = parseFloat((setWeight - 45) / 2);
 
-  var totalSide = parseFloat(0);
-  
   // build totalSide up to goalSide via adding (plate)s to sidePlates
-  // subtract from remainingPlates with each (plate) used
-  // sidePlates = list of (plate)s to use, in order
-  var sidePlates = [];
-
   while ( totalSide <= goalSide, remainingPlates.length > 0 ) {
     var plate = parseFloat(remainingPlates[0]);
     if ( totalSide + plate <= goalSide ) {
@@ -38,9 +34,7 @@ function weightCalc(setWeight)
     remainingPlates.shift();
   }
   
-  // return the per-side total and the list of plates
-  var sideWeight = [totalSide, sidePlates]
-  return sideWeight;
+  return sidePlates;
 }
 
 function weightPrint() {
@@ -49,7 +43,7 @@ function weightPrint() {
     // verify that the stored set total matches the number listed
     var setSide = liftingSets[0];
     if ( setSide == workSheet.getRange((i+5),6).getMergedRanges()[0].getCell(1, 1).getValue()) {
-      var setStack = weightCalc(liftingSets[0])[1];
+      var setStack = weightCalc(setSide);
       for (var j = 0 ; j < setStack.length; j++) {
         var cell = workSheet.getRange((i+5),(j+8));
         cell.getMergedRanges()[0].getCell(1, 1).setValue(setStack[j]);
